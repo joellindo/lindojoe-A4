ef151f8aec378b4c0e880f2dfe200cba7f73d8ae
joellindo <52580005+joellindo@users.noreply.github.com>
2019-08-09 20:37:30 -0400

    Add files via upload

---
+++ assets/data/myprofile.ts
@@ -0,0 +1,5 @@
+import {Profile} from '../../app/lindojoe';
+
+export const MYPROFILE: Profile = {
+    snumber: '991492183', loginname: 'lindojoe', name: 'Joel Lindo', campus: 'Trafalgar', assignment:'Assignment 4'
+};
